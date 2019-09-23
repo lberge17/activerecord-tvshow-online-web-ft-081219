8dc586aaf271898a974c6b7f79b37b9a743df2cf
Laura Berge <lauraeberge@gmail.com>
2019-09-23 01:09:08 +0000

    Automatically backed up by Learn

---
+++ db/migrate/001_create_shows.rb
@@ -0,0 +1,10 @@
+class CreateShows < ActiveRecords::Migration[5.1]
+  def change
+    create_table :shows do |t|
+      t.string :name
+      t.string :network
+      t.string :day
+      t.integer :rating
+    end
+  end
+end
